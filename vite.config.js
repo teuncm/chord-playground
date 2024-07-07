359fffe60847ea6860737a55f450fb8578c59821
teuncm <teun.c.mathijssen@gmail.com>
2024-07-07 17:59:33 +0200

    Add full repo url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'https://teuncm.github.io/chord-playground/',
   plugins: [vue()],
 })
