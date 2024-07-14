c37645233c672c6940881553788a322434628541
teuncm <teun.c.mathijssen@gmail.com>
2024-07-14 16:42:22 +0200

    Remove Pinia references

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,8 @@
 
 <script>
 import ActionContainer from './components/ActionContainer.vue';
-import KeyContainer from './components/KeyContainer.vue';
-import ScaleContainer from './components/ScaleContainer.vue';
 import NoteContainer from './components/NoteContainer.vue';
 import Divider from './components/Divider.vue';
-import Test from './components/Test.vue';
 import ChordContainer from './components/ChordContainer.vue';
 import ShiftContainer from './components/ShiftContainer.vue';
 
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,31 +1,4 @@
-import { defineStore } from 'pinia';
-import { Audio, SynthState } from '../audioState';
+import { SynthState } from '../audioState';
 import { reactive } from 'vue';
-
-export const useAudioStore = defineStore('audio', {
-  state: () => ({
-    audioInstance: null
-  }),
-  actions: {
-    initializeAudio() {
-      this.audioInstance = new Audio();
-    },
-    getChord() {
-      return this.audioInstance.chord;
-    },
-    setChord(chord) {
-      this.audioInstance.chord = chord;
-    }
-  }
-});
-
-// export const useSynthStore = defineStore('synth', {
-//   state: () => ({
-//     synthState: SynthState
-//   }),
-//   actions: {
-
-//   }
-// });
 
 export const synthState = reactive(new SynthState());
